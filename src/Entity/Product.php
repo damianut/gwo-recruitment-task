c459b54206ef5f920c0a3405d4ffaf42ead4d657
damianut <damianas1999@gmail.com>
2020-07-27 20:53:02 +0200

    Creating Product class

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,5 +6,33 @@ namespace Recruitment\Entity;
 
 class Product
 {
-    
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int Product unit price
+     */
+    private $unitPrice;
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setUnitPrice(int $unitPrice): void
+    {
+        //Tutaj wyrzuć wyjątek
+    }
+
+    public function getUnitPrice(): int
+    {
+        return $this->unitPrice;
+    }
 }
